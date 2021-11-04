21be5ff58102f6f8815aa375138e8bb5996a07c7
totori0908 <to_asdf@yahoo.co.jp>
2021-11-04 21:24:33 +0900

    並列処理をできるように

--- run.sh
+++ run.sh
@@ -1,16 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
 cd `dirname $0`
 g++ -std=gnu++1z -I . -O2 -Wall -Wfatal-errors -Wextra -W main.cpp
 rm -rf scores
 mkdir scores
 
+st=0
+en=20
+procs=0
+small=0
+
+calc() {
+  ~/.cargo/bin/cargo run --release --bin tester ./a.out < in/$1.txt > out/$1.txt 2> scores/$1.txt
+}
+
+export -f calc
+
 # ~/.cargo/bin/cargo run --release --bin tester ./a.out < in/0000.txt > out/0000.txt
     
-for i in `seq 0 9`
-do
-~/.cargo/bin/cargo run --release --bin tester ./a.out < in/000$i.txt > out/000$i.txt 2> scores/000$i.txt
-done
+# for i in `seq 0 9`
+# do
+# ~/.cargo/bin/cargo run --release --bin tester ./a.out < in/000$i.txt > out/000$i.txt 2> scores/000$i.txt
+# done
+
+seq -f '%04g' $st $en | xargs -n1 -P$procs -I{} bash -c "calc {}"
 
 
 python3 scoreSum.py
